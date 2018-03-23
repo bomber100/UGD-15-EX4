e41b6bda823e1ba4263360988dfcdd6f20ed2354
Aleksej Tolkachov <aleksej.tolkachov@gmail.com>
2018-03-23 21:29:17 +0200

    wall modifications

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+    Material m_Material;
+
+	void Start () {
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.color = new Color(0.4f, 0.78f, 0.934f, 1.0f);
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour {
+
+    public float speed;
+//<<<<<< HEAD
+    public float jump;
+//=======
+//>>>>>>> b1e36a1871bb26ec8bd9f78debf7b4647705194b
+    private Rigidbody rb;
+	
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+//<<<<<<< HEAD
+//=======
+    private void Update()
+    {
+        
+    }
+//>>>>>>> b1e36a1871bb26ec8bd9f78debf7b4647705194b
+}
